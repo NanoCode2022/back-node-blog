6ec3c868334887b5b50f3c8413ced06a72c2d391
Nahuel <nanocode2022@gmail.com>
2024-06-25 14:00:20 -0300

    add: cors function

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 import express, { json } from "express";
 import 'dotenv/config'
 import blogRouter from "./routes/blogRoute.js"
+import cors from 'cors'
 const app = express()
+app.use(cors())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
